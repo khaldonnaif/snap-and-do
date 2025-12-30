dfe7cc0749c2bd857b22f580e01a115fcd283c8f
Plectocron <khaldonnaif@gmail.com>
2025-12-30 18:39:49 +0800

    feat(sidepanel): Initialize SidePanel component (for #1)

---
+++ src/components/layouts/SidePanel.jsx
@@ -0,0 +1,16 @@
+/**
+ * @param {boolean} isOpen - A state indicating whether or not this component should open
+ * @param {string} width - The width of the side panel 
+ */
+function SidePanel({
+  isOpen=false,
+  width="w-64"
+}) {
+  return(
+    <div>
+
+    </div>
+  );
+}
+
+export default SidePanel;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "../components/layouts/Header.jsx";
 import MainLayout from "../components/layouts/MainLayout.jsx";
+import SidePanel from "../components/layouts/SidePanel.jsx";
 import Button from "../components/common/Button.jsx";
 import { Link } from "react-router-dom";
 
@@ -8,34 +9,36 @@ function Home() {
   const [aboutIsHovered, setAboutIsHovered] = useState(false);
 
   return (
-  <MainLayout>
-    <Header bgClass="bg-slate-dark" bordered={false}>
-      <h1 className="text-4xl font-bold">
-        SnapAndDo
-      </h1>
-      <div className="flex justify-center items-center gap-12">
-        <Button
-          bgColor="bg-purple-muted"
-          bordered={false}
-          className="rounded-3xl"
-        >
-          + Add New Task
-        </Button>
-        <div
-          onMouseEnter={() => setAboutIsHovered(true)}
-          onMouseLeave={() => setAboutIsHovered(false)}
-        >
-          <Link to="/About">
-            <img 
-              src={aboutIsHovered ? "/assets/about/exclamation-point/light-gray-exclamation-point.svg" : "/assets/about/exclamation-point/gray-exclamation-point.svg"} 
-              className="size-8"
-            />
-          </Link>
-        </div>
-      </div>
-
-    </Header>
-  </MainLayout>
+    <div className="relative">
+      <SidePanel></SidePanel>
+      <MainLayout>
+        <Header bgClass="bg-slate-dark" bordered={false}>
+          <h1 className="text-4xl font-bold">
+            SnapAndDo
+          </h1>
+          <div className="flex justify-center items-center gap-12">
+            <Button
+              bgColor="bg-purple-muted"
+              bordered={false}
+              className="rounded-3xl"
+            >
+              + Add New Task
+            </Button>
+            <div
+              onMouseEnter={() => setAboutIsHovered(true)}
+              onMouseLeave={() => setAboutIsHovered(false)}
+            >
+              <Link to="/About">
+                <img 
+                  src={aboutIsHovered ? "/assets/about/exclamation-point/light-gray-exclamation-point.svg" : "/assets/about/exclamation-point/gray-exclamation-point.svg"} 
+                  className="size-8"
+                />
+              </Link>
+            </div>
+          </div>
+        </Header>
+      </MainLayout>      
+    </div>
   );
 }
 
