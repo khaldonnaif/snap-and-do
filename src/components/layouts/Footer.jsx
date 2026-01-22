1a784102245f26e7ff8075f330566243a977cecb
Plectocron <khaldonnaif@gmail.com>
2026-01-22 11:56:43 +0800

    refactor(Header, Footer): simplify props, use semantic CSS classes (refs #13)

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,31 +1,14 @@
 /**
- * @param {string} className - Custom styling that isn't accounted for (e.g, animations)
- * @param {string} height - A class that defines the height (e.g., h-20)
- * @param {string} bgClass - Tailwind background utilities, solid or gradient (e.g., "bg-black" or "bg-gradient-to-t from-black to-white")
- * @param {string} textColor - A class that determines the text's color (e.g., text-white)
- * @param {string} borderThickness - Choose a valid tailwind border-size class (e.g., border-2)
- * @param {string} borderColor - Choose a valid tailwind border color class (e.g., border-white)
- * @param {boolean} bordered - Decides whether or not to show the borders at all (e.g., true)
+ * A footer component that accepts CSS classes for styling
+ * 
+ * @param {string} className - Additional CSS classes
  */
 function Footer({
   children,
-  height="h-20",
-  bgClass="bg-black",
-  textColor="text-white",
-  borderThickness="border-2",
-  borderColor="border-white",
-  bordered=true,
   className=""
 }) {
   return(
-    <div className={`
-      w-full 
-      ${height}
-      ${className}
-      ${bgClass}
-      ${textColor}
-      ${bordered ? `${borderThickness} ${borderColor}` : ""}
-      `}>
+    <div className={`footer ${className}`}>
       {children}
     </div>
   );
--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,33 +1,14 @@
 /**
- * @param {string} className - Custom styling that isn't accounted for (e.g, animations)
- * @param {string} height - A class that defines the height (e.g., h-20)
- * @param {string} bgClass - Tailwind background utilities, solid or gradient (e.g., "bg-black" or "bg-gradient-to-t from-black to-white")
- * @param {string} textColor - A class that determines the text's color (e.g., text-white)
- * @param {string} borderThickness - Choose a valid tailwind border-size class (e.g., border-2)
- * @param {string} borderColor - Choose a valid tailwind border color class (e.g., border-white)
- * @param {boolean} bordered - Decides whether or not to show the borders at all (e.g., true)
+ * A header component that accepts CSS classes for styling
+ * 
+ * @param {string} className - Additional CSS classes
  */
 function Header({
   children,
-  height="h-20",
-  bgClass="bg-black",
-  textColor="text-white",
-  borderThickness="border-2",
-  borderColor="border-white",
-  bordered=true,
   className=""
 }) {
   return (
-    <div className={`
-      ${bgClass}
-      ${textColor} 
-      w-full 
-      ${height} 
-      px-12 
-      ${bordered ? `${borderThickness} ${borderColor}` : ""}
-      flex items-center justify-between 
-      ${className}
-    `}>
+    <div className={`header ${className}`}>
       {children}
     </div>
   );
