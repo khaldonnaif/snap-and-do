56df37e5c20a0f0326f02b9a0bc5c5a14ca9fdcf
Plectocron <khaldonnaif@gmail.com>
2026-01-22 11:48:08 +0800

    refactor(Circle): simplify props, use CSS classes (refs #13)

--- src/components/common/Circle.jsx
+++ src/components/common/Circle.jsx
@@ -1,35 +1,23 @@
 /**
- * @param {string} className - Custom styling that isn't accounted for (e.g, animations)
- * @param {string} bgColor - Choose a valid tailwind background color class (e.g., bg-black)
- * @param {"sm" | "md" | "lg" | "xl" | "2xl"} size - Sets static size
- * @param {string} borderThickness - Choose a valid tailwind border-size class (e.g., border-2)
- * @param {string} borderColor - Choose a valid tailwind border color class (e.g., border-white)
- * @param {boolean} bordered - Decides whether or not to show the borders at all (e.g., true)
+ * A circular component that accepts CSS classes for styling
+ * 
+ * @param {string} size - Predefined size (e.g., "circle-sm", "circle-md", "circle-lg", "circle-xl", "circle-2xl")
+ * @param {string} className - Additional CSS classes
  */
 function Circle({
-  bgColor="bg-white",
   size="md",
-  borderThickness="border-2",
-  borderColor="border-black",
-  bordered=true,
   className=""
 }) {
   const circleSize = {
-    sm: "w-4 h-4",
-    md: "w-8 h-8",
-    lg: "w-12 h-12",
-    xl: "w-16 h-16",
-    "2xl": "w-32 h-32"
+    sm: "circle-sm",
+    md: "circle-md",
+    lg: "circle-lg",
+    xl: "circle-xl",
+    "2xl": "circle-2xl"
   };
 
   return (
-    <div className={`
-      rounded-full
-      ${bgColor} 
-      ${circleSize[size]} 
-      ${bordered ? `${borderThickness} ${borderColor}` : ""}
-      ${className}
-      `} />
+    <div className={`${circleSize[size]} ${className}`} />
   );
 }
 
